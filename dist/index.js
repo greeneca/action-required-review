63842e1bacd7d54170f3e2380fc8d98baa6ca5d2
zinigor <zinigor@users.noreply.github.com>
2025-03-03 12:55:07 +0000

    Lock file maintenance (#42180)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13631106056
    
    Upstream-Ref: Automattic/jetpack@665e8a1cf9e9bc702d47263f74934e1a092bc414

--- dist/index.js
+++ dist/index.js
@@ -3565,7 +3565,7 @@ module.exports = __toCommonJS(dist_src_exports);
 var import_universal_user_agent = __nccwpck_require__(7125);
 var import_before_after_hook = __nccwpck_require__(6223);
 var import_request = __nccwpck_require__(2518);
-var import_graphql = __nccwpck_require__(4135);
+var import_graphql = __nccwpck_require__(6859);
 var import_auth_token = __nccwpck_require__(8953);
 
 // pkg/dist-src/version.js
@@ -4086,7 +4086,7 @@ var endpoint = withDefaults(null, DEFAULTS);
 
 /***/ }),
 
-/***/ 4135:
+/***/ 6859:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 "use strict";
@@ -4110,18 +4110,18 @@ var __copyProps = (to, from, except, desc) => {
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // pkg/dist-src/index.js
-var dist_src_exports = {};
-__export(dist_src_exports, {
+var index_exports = {};
+__export(index_exports, {
   GraphqlResponseError: () => GraphqlResponseError,
   graphql: () => graphql2,
   withCustomRequest: () => withCustomRequest
 });
-module.exports = __toCommonJS(dist_src_exports);
+module.exports = __toCommonJS(index_exports);
 var import_request3 = __nccwpck_require__(2518);
 var import_universal_user_agent = __nccwpck_require__(7125);
 
 // pkg/dist-src/version.js
-var VERSION = "7.1.0";
+var VERSION = "7.1.1";
 
 // pkg/dist-src/with-defaults.js
 var import_request2 = __nccwpck_require__(2518);
@@ -4169,8 +4169,7 @@ function graphql(request2, query, options) {
       );
     }
     for (const key in options) {
-      if (!FORBIDDEN_VARIABLE_OPTIONS.includes(key))
-        continue;
+      if (!FORBIDDEN_VARIABLE_OPTIONS.includes(key)) continue;
       return Promise.reject(
         new Error(
           `[@octokit/graphql] "${key}" cannot be used as variable name`
