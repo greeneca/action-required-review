719bfde10fc5152103e1a96358f16438e9b2d121
Charles Greene <charles.greene@redspace.com>
2025-08-25 11:53:33 -0300

    ROKU-12294 - update

--- dist/index.js
+++ dist/index.js
@@ -36843,7 +36843,7 @@ async function requestReviewer( teams ) {
 		teams = teams.filter( team => team !== author );
 	}
 
-    const existingReviewers = await octokit.rest.pulls.reviewers.list({
+    const existingReviewers = await octokit.rest.pulls.reviewers({
         owner: owner,
         repo: repo,
         pull_number: pr,
