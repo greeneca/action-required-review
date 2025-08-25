9cf4ca866c492f260d950e1d61822bf5d9a02929
Charles Greene <charles.greene@redspace.com>
2025-08-25 11:52:13 -0300

    ROKU-12294 - typo

--- dist/index.js
+++ dist/index.js
@@ -36843,7 +36843,7 @@ async function requestReviewer( teams ) {
 		teams = teams.filter( team => team !== author );
 	}
 
-    const existingReviewers = await github.rest.pulls.reviewers.list({
+    const existingReviewers = await octokit.rest.pulls.reviewers.list({
         owner: owner,
         repo: repo,
         pull_number: pr,
